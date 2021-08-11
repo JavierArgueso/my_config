b6e99fc2400f09b7c39320855573e390eb422a8f
JavierArgueso <javierarguesosoto@gmail.com>
2021-08-11 18:49:04 +0200

    [update] mejora en el script de autoinstall

--- autoconfig.sh
+++ autoconfig.sh
@@ -4,8 +4,21 @@
 echo "\nActualizando sistema"
 sudo apt update && sudo apt upgrade -y
 
+echo "\nInstalando repositorios"
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+
 echo "\nInstalando programas"
-sudo apt install neovim zsh w3m-img imagemagick xdotool curl bspwm sxhkd rofi htop suckless-tools
+sudo apt install neovim zsh w3m-img imagemagick xdotool curl bspwm sxhkd rofi htop suckless-tools nodejs npm yarm
+
+echo "\nDocker"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG
 
 # Coniguracion de git
 echo "\nConfigurando git"
@@ -15,34 +28,27 @@ git config --global user.name "JavierArgueso"
 # Widget Desktop
 echo "\nDescargando e instalando el visor de escritorios"
 git clone https://github.com/wsdfhjxc/virtual-desktop-bar.git
-cd $HOME/virtual-desktop-bar/scripts/
-./install-dependencies-ubuntu.sh
-./install-applet.sh
-cd $HOME
+~/virtual-desktop-bar/scripts/install-dependencies-ubuntu.sh
+~/virtual-desktop-bar/scripts/install-applet.sh
 
 # Configuracion de las aplicaciones
 echo "\nConfigurando nvim"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "\nCopiando el inicio con Plasma y BSPWM en /usr/share/xsessions"
-cd ./my_config
-sudo cp ./plasmaBSPWM.desktop /usr/share/xsessions/
+sudo cp ~/my_config/plasmaBSPWM.desktop /usr/share/xsessions/
 
 echo "\nCreando carpeta y copiando conguracion de bspwm"
-mkdir $HOME/.config/bspwm
-cp ./bspwm/bspwmrc $HOME/.config/bspwm/
+cp -r ~/my_config/bspwm/ ~/.config/bspwm/
 
 echo "\nCreando carpeta y copiando configuracion de sxhkd"
-mkdir $HOME/.config/sxhkd
-cp ./sxhkd/sxhkdrc $HOME/.config/sxhkd/
+cp -r ~/my_config/sxhkd/ ~/.config/sxhkd/
 
 echo "\nGuardando los wallpapers"
 cp -r ./wallpapers $HOME/.local/share/
 
 echo "\nCreando carpeta y copiando dotFiles de nvim"
-mkdir $HOME/.config/nvim
-cp ./nvim/init.vim $HOME/.config/nvim/
-cp ./nvim/.vimrc $HOME/.config/nvim/
+cp -r ~/my_config/nvim/ ~/.config/nvim/
 
 echo "\nA continuación se van a instalar los plugins de nvim"
 nvim +PlugInstall
@@ -55,9 +61,9 @@ echo "coc-css"
 echo "coc-json"
 echo "coc-tsserver"
 
+nvim +"CocInstall coc-json coc-tsserver"
+
+echo "\nFalta por instalar code y slimbook batery ademas de poner bonito"
+
 echo "\nConfigurando zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
- 
-
-
-
